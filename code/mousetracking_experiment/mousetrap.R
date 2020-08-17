56c1cb214dce7044691b8a9f7fd7246d72b61560
mwishart <55006288+mwishart@users.noreply.github.com>
2020-08-17 16:48:19 -0400

    code updates

--- code/mousetracking_experiment/mousetrap.R
+++ code/mousetracking_experiment/mousetrap.R
@@ -1,10 +1,14 @@
-study <- test2
 
-study
 library(tidyr)
 library(mousetrap)
+library(ggplot2)
+library(grid)
+library(png)
+#remove all rows w/o mouse tracking data
+#or else mousetrap will give errors
+cleanstudy <- fullexperiment[-c(1,5,6,7,8),]  
 mousedata <- mt_import_mousetrap(
-  study,
+  cleanstudy,
   xpos_label = "xpos",
   ypos_label = "ypos",
   timestamps_label = "timestamps",
@@ -15,16 +19,14 @@ mousedata <- mt_import_mousetrap(
   verbose = FALSE
 )
 
-mousedata$trajectories
-mt_time_normalize(
-  mousedata,
-  use = "trajectories",
-  save_as = "tn_trajectories",
-  dimensions = c("xpos", "ypos"),
-  timestamps = "timestamps",
-  nsteps = 200,
-  verbose = FALSE
-)
-mt_plot(mousedata, use = "trajectories", x = "xpos", y="ypos", color = "mt_id")
+
+mt_plot(mousedata, use = "trajectories", x = "xpos", y="ypos", color = "mt_id") #plot of all screens
 mt_heatmap(mousedata, use="trajectories",dimensions = c("xpos","ypos"),
            filename = NULL, plot_dims = FALSE, verbose = TRUE)
+mt_plot_per_trajectory(mousedata, file = "trajectories.pdf", use = "trajectories",
+                       x = "xpos",
+                       y = "ypos",) #saves individual screens to a pdf file
+mousevel <- mt_derivatives(mousedata,use = "trajectories",dimensions = c("xpos", "ypos"),
+               timestamps = "timestamps",)
+
+
